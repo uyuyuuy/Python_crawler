201794c692c483b93ac93fcf83c6e8713e91510d
uyuyuuy <842276675@qq.com>
2018-03-16 18:43:10 +0800

    优化
    文件下载和文件下载地址封装成方法

--- robot.py
+++ robot.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import sys, io, os, bs4, requests, re, time, threading
+from lxml import etree
 
 # sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') #改变标准输出的默认编码
-
 
 # 抓取页面内容函数
 def down_html(web_url, next_page_tag):
@@ -27,42 +27,27 @@ def down_html(web_url, next_page_tag):
     imgObj = html_bs.select('img')
     for img in imgObj:
         img_src = img.get('src')
-        if re.match('//',img_src):
-            img_src = 'http:' + img_src
-        if not (re.match('http://',img_src) or re.match('https://',img_src)):
-            img_src = home_url + img_src
+        img_src = get_real_url(img_src)
         print(img_src)
 
-        dir_path = 'files/' + '/'.join(img_src.split('/')[2:-1])
-        img_path = dir_path + '/' + img_src.split('/')[-1]
+        image_file_path = down_file(img_src, file_root)
 
-        html_text = html_text.replace(img.get('src'),'../'+img_path,1)
-
-        os.makedirs(dir_path, 0o777, True)  # True表示忽略已经存在的文件夹
-        imgBox = open(img_path,'wb')
-        imgBox.write(requests.get(img_src).content)
+        html_text = html_text.replace(img.get('src'),'../'+image_file_path,1)
 
 
     # 下载css
     cssObj = html_bs.select('link')
     for css in cssObj:
-        css_rel = css.get('rel')
+        css_rel = css.get('rel') #奇怪，为什么是数组
+        # print(css_rel)
         css_href = css.get('href')
         if css_rel[0] == 'stylesheet' and css_href:
-            if re.match('//',css_href):
-                css_href = 'http:' + css_href
-            elif re.match('/',css_href):
-                css_href = home_url + css_href
+            css_href = get_real_url(css_href)
             print(css_href)
 
-            css_dir_path = 'files/' + '/'.join(css_href.split('/')[2:-1])
-            css_file_path = css_dir_path + '/' + css_href.split('/')[-1].split('?')[0]
+            css_file_path = down_file(css_href, file_root)
 
             html_text = html_text.replace(css.get('href'), '../' + css_file_path, 1)
-
-            os.makedirs(css_dir_path, 0o777, True)  # True表示忽略已经存在的文件夹
-            cssBox = open(css_file_path,'wb')
-            cssBox.write(requests.get(css_href).content)
 
 
     # 下载js
@@ -70,20 +55,12 @@ def down_html(web_url, next_page_tag):
     for js in jsObj:
         js_src = js.get('src')
         if js_src:
-            if re.match('//',js_src):
-                js_src = 'http:' + js_src
-            elif re.match('/',js_src):
-                js_src = home_url + js_src
+            js_src = get_real_url(js_src)
             print(js_src)
 
-            js_dir_path = 'files/' + '/'.join(js_src.split('/')[2:-1])
-            js_file_path = js_dir_path + '/' + js_src.split('/')[-1].split('?')[0]
+            js_file_path = down_file(js_src, file_root)
 
             html_text = html_text.replace(js.get('src'), '../' + js_file_path, 1)
-
-            os.makedirs(js_dir_path, 0o777, True)  # True表示忽略已经存在的文件夹
-            jsBox = open(js_file_path,'wb')
-            jsBox.write(requests.get(js_src).content)
 
 
     # 处理目录url
@@ -111,7 +88,7 @@ def down_html(web_url, next_page_tag):
         next_page_url = next_page_obj[0].get('href')
         if re.match('/',next_page_url):
             next_page_url = home_url + next_page_url
-        down_html(next_page_url, next_page_tag)
+        # down_html(next_page_url, next_page_tag)
     else:
         for thread in thread_list:
             thread.join()
@@ -132,6 +109,113 @@ def create_html_file(file_name, content):
 
 
 
+# 抓取页面内容函数
+def down_html_lxml(web_url, next_page_xpath):
+    global index
+    # 获取页面内容
+    web_res = requests.get(web_url)
+    web_res.encoding = 'UTF-8'  # 改变编码
+
+    try:
+        web_res.raise_for_status()
+    except Exception as err:
+        print('Error:' + err)
+        sys.exit('Over')
+
+    html_text = web_res.text
+    html_etree = etree.HTML(html_text)
+
+    # 下载图片
+    imgObj = html_etree.xpath('//img')
+    for img in imgObj:
+        img_src = img.get('src')
+        img_src = get_real_url(img_src)
+        print(img_src)
+
+        image_file_path = down_file(img_src, file_root)
+
+        html_text = html_text.replace(img.get('src'), '../' + image_file_path, 1)
+
+    # 下载css
+    cssObj = html_etree.xpath('//link')
+    for css in cssObj:
+        css_rel = css.get('rel')
+        css_href = css.get('href')
+        if css_rel == 'stylesheet' and css_href:
+            css_href = get_real_url(css_href)
+            print(css_href)
+
+            css_file_path = down_file(css_href, file_root)
+
+            html_text = html_text.replace(css.get('href'), '../' + css_file_path, 1)
+
+    # 下载js
+    jsObj = html_etree.xpath('//script')
+    for js in jsObj:
+        js_src = js.get('src')
+        if js_src:
+            js_src = get_real_url(js_src)
+            print(js_src)
+
+            js_file_path = down_file(js_src,file_root)
+
+            html_text = html_text.replace(js.get('src'), '../' + js_file_path, 1)
+
+
+    # 处理目录url
+    contentsObj = html_etree.xpath("//*[@id='leftcolumn']/a")
+    for contents in contentsObj:
+        html_text = html_text.replace(contents.get('href'), contents.get('href').split('/')[-1], 1)
+
+    # 创建线程写入文件
+    # 生成网页文件
+    file_name_re = re.search(r'.*/([a-zA-Z0-9-.]+)', web_url)
+    file_name = file_name_re.group(1)
+
+    print('Start:' + str(index))
+    thread_list = []
+    threadObj = threading.Thread(target=create_html_file, args=(file_name, html_text.encode('utf-8')))
+    thread_list.append(threadObj)
+    threadObj.start()
+
+    index = int(index) + 1
+
+    # 下一页
+    next_page_obj = html_etree.xpath(next_page_xpath)
+    if next_page_obj:
+        next_page_url = next_page_obj[0].get('href')
+        if re.match('/', next_page_url):
+            next_page_url = home_url + next_page_url
+        down_html_lxml(next_page_url, next_page_xpath)
+    else:
+        for thread in thread_list:
+            thread.join()
+        print('Over')
+
+
+#处理url
+def get_real_url(url):
+    if re.match('//', url):
+        url = 'http:' + url
+    elif re.match('/', url):
+        url = home_url + url
+    return url
+
+
+
+#下载文件
+def down_file(url,root_path):
+    dir = root_path + '/' + '/'.join(url.split('/')[2:-1])
+    file_path = dir + '/' + url.split('/')[-1].split('?')[0]
+
+    os.makedirs(dir, 0o777, True)  # True表示忽略已经存在的文件夹
+    file = open(file_path, 'wb')
+    file.write(requests.get(url).content)
+
+    return file_path
+
+
+
 # 生成目录文件（暂时不用）
 # def create_contents():
 #     index_file = open(os.path.join('.','html','index.html'),'w')
@@ -146,6 +230,9 @@ def create_html_file(file_name, content):
 
 
 
+
+
+############### 执行程序 ###############
 #创建目录
 if not os.path.isdir('html'):
     os.mkdir('html',0o777)
@@ -156,9 +243,14 @@ if not os.path.isdir('files'):
 home_url = 'http://www.runoob.com'
 web_url = 'http://www.runoob.com/nodejs/nodejs-tutorial.html'
 next_page_tag = ".next-design-link a"
+next_page_xpath = "//div[@class='next-design-link']/a"
 index = '1'
+file_root = 'files'
+
 
 down_html(web_url, next_page_tag)
+# down_html_lxml(web_url, next_page_xpath)
+
 
 sys.exit('Exit')
 
