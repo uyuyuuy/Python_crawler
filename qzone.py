9e0eb53630a13ae157f15cfef3d44e24044fb452
uyuyuuy <842276675@qq.com>
2018-03-19 18:36:12 +0800

    优化

--- qzone.py
+++ qzone.py
@@ -10,7 +10,7 @@ from selenium import webdriver
 
 print('开始执行程序')
 qq = '842276675'
-driver = webdriver.Firefox(executable_path='/Users/xiangdong/Downloads/geckodriver')
+driver = webdriver.Chrome()
 
 #打开qq空间相册页面
 print('打开相册列表页面，该页面会自动跳转到登录页面')
@@ -20,7 +20,7 @@ driver.maximize_window()
 time.sleep(2)
 driver.switch_to.frame('login_frame')
 print('点击快捷登录图标')
-driver.find_element_by_xpath("//div[@id='qlogin_list']/a[@class='face']").click()
+driver.find_elements_by_xpath("//div[@id='qlogin_list']/a[@class='face']")[1].click()
 
 time.sleep(15)
 
@@ -28,38 +28,68 @@ driver.switch_to.default_content()
 js = "var q=document.documentElement.scrollTop=10000"
 print('页面滑动到底部')
 driver.execute_script(js)
-time.sleep(3)
+time.sleep(2)
 driver.switch_to.frame('app_canvas_frame')
 
 lists = driver.find_elements_by_xpath("//a[@class='c-tx2 js-album-desc-a']")
 album_number = len(lists)
 print(len(lists))
-time.sleep(3)
-index = 20
+time.sleep(2)
+index = 7
 while index < album_number:
-    tmp_floot = index / 21
+    driver.switch_to.default_content()
+    js = "var q=document.documentElement.scrollTop=0"
+    driver.execute_script(js)
+    time.sleep(1)
+
+    js = "var q=document.documentElement.scrollTop=100000"
+    driver.execute_script(js)
+    time.sleep(1)
+
+    tmp_floot = index / 8
     page = int(math.floor(tmp_floot))
 
-    driver.switch_to.default_content()
-    js = "var q=document.documentElement.scrollTop=" + str(425 + 640*page)
+    js = "var q=document.documentElement.scrollTop=" + str(445 + 240*page)
     driver.execute_script(js)
-    print('相册列表第' + str(page) + '页')
     print(js)
-    time.sleep(8)
+    print('相册列表第' + str(page) + '页')
+    time.sleep(2)
+
     driver.switch_to.frame('app_canvas_frame')
     lists = driver.find_elements_by_xpath("//a[@class='c-tx2 js-album-desc-a']")
     print(len(lists))
     print(lists[index].text)
+
+    #进入相册详情页面
     lists[index].click()
-    time.sleep(3)
-    driver.switch_to.default_content()
-    js = "var q=document.documentElement.scrollTop=425"
-    print('页面向上滑动425像素')
+    time.sleep(1)
+    js = "var q=document.documentElement.scrollTop=0"
     driver.execute_script(js)
-    time.sleep(3)
+    time.sleep(2)
+
+    #定位到图片列表
+    driver.switch_to.default_content()
+
+    js = "var q=document.documentElement.scrollTop=10000"
+    print('页面滑到底部')
+    driver.execute_script(js)
+    time.sleep(2)
+
+    js = "var q=document.documentElement.scrollTop=340"
+    print('页面向上滑动340像素')
+    driver.execute_script(js)
+    time.sleep(2)
+
+    driver.switch_to.frame('app_canvas_frame')
+    images = driver.find_elements_by_xpath("//ul[@class='list j-pl-photolist-ul']/li")
+    
+
+
+
+
     print('返回相册列表页面')
     driver.back()
-    time.sleep(5)
+    time.sleep(1)
     index += 1
 
 driver.close()
